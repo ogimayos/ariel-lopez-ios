3c9dee1bd72fca2848d8b232b2074abac546d2fe
ogimayos <cbba75@hotmail.com>
2015-06-26 16:58:37 -0400

    first commit

---
+++ ventasMoviles/Cliente.swift
@@ -0,0 +1,13 @@
+//
+//  Cliente.swift
+//  ventasMoviles
+//
+//  Created by internet on 5/23/15.
+//  Copyright (c) 2015 katarzis. All rights reserved.
+//
+
+import UIKit
+
+class Cliente: NSManagedObject {
+   
+}
